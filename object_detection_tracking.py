71b67680e5248f0776960f32f825c8ccab420175
poisonshade <imildositoe@gmail.com>
2025-06-18 18:22:58 +0200

    block to detect and cluster the objects using DBSCAN

--- object_detection_tracking.py
+++ object_detection_tracking.py
@@ -18,3 +18,22 @@ def preprocess_frame(df):
     df.loc[:, 'INTENSITY_NORM'] = StandardScaler().fit_transform(df[['INTENSITY']])
     return df[['X', 'Y', 'Z', 'DISTANCE_NORM', 'INTENSITY_NORM']]
 
+# This snippet is to detect the objects and cluster them using DBSCAN
+def detect_objects(df, eps=1.0, min_samples=10):
+    coords = df[['X', 'Y', 'Z']].values
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(coords)
+    df['cluster'] = db.labels_
+    return df[df['cluster'] != -1]
+
+def get_object_centroids(df):
+    return df.groupby('cluster')[['X', 'Y', 'Z']].mean().reset_index()
+
+all_detections = []
+
+for i, frame in enumerate(dsc.all_frames):
+    clean_frame = preprocess_frame(frame)
+    clustered = detect_objects(clean_frame)
+    centroids = get_object_centroids(clustered)
+    centroids['frame_index'] = i
+    all_detections.append(centroids)
+
