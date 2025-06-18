3eb2fe232f6021febdc447f95a677f9f07353e37
poisonshade <imildositoe@gmail.com>
2025-06-18 18:11:18 +0200

    data_sanity_check creation and treat distance intensity

--- object_detection_tracking.py
+++ object_detection_tracking.py
@@ -6,13 +6,15 @@ from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 from scipy.optimize import linear_sum_assignment
 import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
 
 dsc
 
 # Function to remove ground points, adjust, and normalize distance intensity
 def preprocess_frame(df):
-    df = df[df['Z'] > -0.5]
-    df['DISTANCE_NORM'] = StandardScaler().fit_transform(df[['DISTANCE']])
-    df['INTENSITY_NORM'] = StandardScaler().fit_transform(df[['INTENSITY']])
+    df = df[df['Z'] > -0.5].copy()
+    df.loc[:, 'DISTANCE_NORM'] = StandardScaler().fit_transform(df[['DISTANCE']])
+    df.loc[:, 'INTENSITY_NORM'] = StandardScaler().fit_transform(df[['INTENSITY']])
     return df[['X', 'Y', 'Z', 'DISTANCE_NORM', 'INTENSITY_NORM']]
 
